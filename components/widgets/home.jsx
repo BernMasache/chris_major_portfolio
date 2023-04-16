25c0d3b6f09ab9c7c468aa3d58694e7160b97eee
BernMasache <bernardmasache@gmail.com>
2023-04-16 18:42:34 +0200

    pages design

--- components/widgets/home.jsx
+++ components/widgets/home.jsx
@@ -4,7 +4,7 @@ import logos from '../../data/logos';
 function HomePage(props) {
     return (
         <div>
-            <div className="relative isolate overflow-hidden bg-gray-100 pt-14 sm:pb-20 h-screen">
+            <div className="relative isolate overflow-hidden bg-gray-100 pt-14 sm:pb-20 pb-8">
                 <img
                     src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
                     alt=""
